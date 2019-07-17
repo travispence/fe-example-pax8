b7a0f2e85374efb326a08262227341210bea269e
Travis Pence <travis.pence@gmail.com>
2019-07-17 16:00:27 -0600

    Add basic Navbar and Footer around Router view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
-    <router-view/>
+    <nav class="navbar has-background-primary" role="navigation" aria-label="main navigation">
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <h1 class="has-text-white title is-4">Pax8 FrontEnd Assignment</h1>
+          </div>
+          <router-link class="navbar-item" to='/'>Home</router-link>
+        </div>
+      </div>
+    </nav>
+    <router-view class="has-background-secondary" />
+    <footer class="footer ">
+      <div class="content has-text-centered">
+        <p>
+          <strong>FrontEnd Homework Project for Pax8 interview</strong> by
+          <a href="https://travispence.com">Travis Pence</a>.
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  @import '~bulma/bulma';
+  @import "~bulma/bulma";
 }
 </style>
