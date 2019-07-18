6310b5747a1c39e9554417e590629e5cd4a06d7a
Travis Pence <travis.pence@gmail.com>
2019-07-17 18:40:27 -0600

    Clean up styling and formatting of company list

--- src/components/companyList.vue
+++ src/components/companyList.vue
@@ -1,35 +1,48 @@
 <template>
-    <div>
-        <slot name="title">  
-        </slot>
-        <ul class="company--list">
-            <slot>
-            <li class="company--list-item" v-for="company in companies" v-bind:key="company.id"> 
-               {{company.id }} (  {{company.name}}  ) - <router-link :to="`/company/edit/${company.id}`">edit</router-link>
-            </li> 
-            </slot>
-        </ul>
-    </div>
+  <div>
+    <slot name="title"></slot>
+    <ul class="company--list">
+      <slot>
+        <li class="company--list-item" v-for="company in companies" v-bind:key="company.id">
+          <div>{{`${company.id} ( ${company.name} )`}} </div>
+          <div>
+            <router-link :to="`/company/edit/${company.id}`">edit</router-link>
+          </div>
+          <div>
+            <router-link :to="`/company/${company.id}`">view</router-link>
+          </div>
+        </li>
+      </slot>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        companies: {
-            type: Array,
-            required: true
-        }
-    }
-}
+  props: {
+    companies: {
+      type: Array,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style>
-    .company--list{
-        list-style-type: none;
-    }
-    .company--list-item {
-        margin: 0;
-        padding: 0;
-    }
-    
+.company--list {
+  list-style-type: none;
+}
+.company--list-item {
+  margin: 0;
+  padding: 0;
+  height: 2rem;
+}
+.company--list-item:nth-child(2n) {
+  background-color: #efefef;
+}
+.company--list-item div {
+    display: inline-block;
+    margin-left: .5rem;
+    padding: .5rem;
+}
 </style>
