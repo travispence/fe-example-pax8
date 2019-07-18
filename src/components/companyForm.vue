27475981e10288e32c757721928d15caa3d9f1ef
Travis Pence <travis.pence@gmail.com>
2019-07-17 18:38:30 -0600

    Implement the @input event for the form component. Notify of all changed fields in an alert

--- src/components/companyForm.vue
+++ src/components/companyForm.vue
@@ -1,16 +1,34 @@
 <template>
-  <form @submit.prevent="onSubmit(form)">
+  <form @submit.prevent="onSubmit">
+     <div class="field">
+      <label class="label">ID</label>
+      <div class="control">
+        <input class="input" :value="form.id" type="text" placeholder="XXXXXX1234" disabled/>
+      </div>
+    </div>
     <div class="field">
       <label class="label">Name</label>
       <div class="control">
-        <input class="input" type="text" placeholder="Pax8" />
+        <input class="input" @input="localForm.name = $event.target.value" :value="form.name" type="text" placeholder="Pax8" />
       </div>
     </div>
 
     <div class="field">
       <label class="label">Domain</label>
       <div class="control">
-        <input class="input" type="text" placeholder="e.g. pax8.com" />
+        <input class="input" @input="localForm.domain = $event.target.value" :value="form.domain" type="text" placeholder="e.g. pax8.com" />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Number of Employees</label>
+      <div class="control">
+        <input class="input" @input="localForm.numberOfEmployees = $event.target.value" :value="form.numberOfEmployees" type="text" placeholder="e.g. 10, 20 etc." />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Subscriptions Per Employee</label>
+      <div class="control">
+        <input class="input" @input="localForm.subscriptionsPerEmployee = $event.target.value" :value="form.subscriptionsPerEmployee" type="text" placeholder="e.g. 1, 5, 25" />
       </div>
     </div>
     <button class="button is-primary" type="submit">Submit</button>
@@ -19,15 +37,26 @@
 
 <script>
 export default {
-  data: () => {
+  data:() => {
     return {
-      form: {}
-    };
+      localForm: {}
+    }
+  },
+  props: {
+    form: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.localForm = {... this.form }; 
+    })
   },
   methods: {
-    onSubmit(form) {
+    onSubmit() {
       //validations
-      this.$emit("submit", form);
+      this.$emit("submit", {old: this.form, new: this.localForm});
     }
   }
 };
--- src/views/EditCompany.vue
+++ src/views/EditCompany.vue
@@ -1,25 +1,45 @@
 <template>
-    <div>
-        <h2>Edit Company</h2>
-        <CompanyForm @submit="handleSubmit"/>
-    </div>
+  <div class="container pt-2">
+    <h2 class="title is-2">Edit Company</h2>
+    <CompanyForm :form="form" @submit="handleSubmit" />
+  </div>
 </template>
 
 <script>
-import CompanyForm from '../components/companyForm'
-export default {
-    components: {
-        CompanyForm
-    },
-    methods: {
-        handleSubmit(form) {
-            alert(form.toString())
-        }
-    }
+import API from '../services/CompanyService';
 
-}
+import CompanyForm from '../components/companyForm.vue';
+
+export default {
+  components: {
+    CompanyForm,
+  },
+  data: () => ({
+    form: {},
+  }),
+  methods: {
+    handleSubmit(payload) {
+      const difference = Object.keys(payload.old).filter(k => (payload.new[k] && payload.old[k]
+       !== payload.new[k]));
+      let out = '';
+      difference.forEach((key) => {
+        out += `The field '${key}' has changed from ${payload.old[key]} to ${payload.new[key]}`;
+        out += '\n';
+      });
+      alert(out);
+    },
+  },
+  mounted() {
+    if (!this.$route.params.id) {
+      console.error('Missing Route Param ID');
+    }
+    const id = parseInt(this.$route.params.id, 10);
+    API.getById(id).then((data) => {
+      this.form = data;
+    });
+  },
+};
 </script>
 
 <style>
-
 </style>
