9e793aa635f018122858da563f0b3af53196a0bb
Travis Pence <travis.pence@gmail.com>
2019-07-17 18:41:48 -0600

    Add FA icons for the increment/decrement and emit the updated value

--- src/components/employeeCounter.vue
+++ src/components/employeeCounter.vue
@@ -1,44 +1,74 @@
 <template>
-    <div>
-        <div class="columns">
-            <div class="column">
-                decrement
-            </div>
-            <div class="column">
-                <span class>Number of Employees - {{count}}</span>
-            </div>
-            <div class="column">
-                increment
-            </div>
-        </div>
-        <div v-show="initialCount !== count">
-            <button class="is-primary">Submit Changes</button>
-        </div>
+  <div class="contiainer counter--container">
+    <h3 class="title is-3">Employee Counter</h3>
+    <p></p>
+    <div class="columns">
+      <div class="column">
+        <a class="button is-danger" @click="decrement()">
+          <span class="icon">
+            <i class="fas fa-minus"></i>
+          </span>
+        </a>
+      </div>
+      <div class="column is-half">
+        <span class>Number of Employees - {{count}}</span>
+      </div>
+      <div class="column">
+        <a class="button is-info" @click="increment()">
+          <span class="icon">
+            <i class="fas fa-plus"></i>
+          </span>
+        </a>
+      </div>
     </div>
+    <div  v-show="(initialCount !== count)">
+        <span class="is-red-text">
+            ** there have been changes made
+        </span>
+      <button class="button is-primary" @click="onSubmit(count)">Save Changes</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data: () => {
-        count: 0
-    },
-    props: {
-        initialCount: {
-            type: Number,
-            required: true
-        }
-    },
-    methods: {
-        onSubmit(count) {
-            this.$emit('updateCount', count)
-        }
-    },
-    mounted() {
-        this.count = this.initialCount || 0;
+  data: () => {
+    return {
+      count: 0
+    };
+  },
+  props: {
+    initialCount: {
+      type: Number,
+      required: true
     }
-}
+  },
+  methods: {
+    onSubmit(count) {
+      this.$emit("updateCount", count);
+    },
+    decrement() {
+      this.count--;
+    },
+    increment() {
+      this.count++;
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+       this.count =  this.count || this.initialCount
+    })
+  },
+  updated() {
+    this.$nextTick(() => {
+      this.count =  this.count || this.initialCount
+    })
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.counter--container {
+  width: 500px;
+}
 </style>
